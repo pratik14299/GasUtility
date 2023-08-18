5fc3d2593a29aa8a0c0ffec24f2e12dbb04f0650
unknown <pratik14299@gmail.com>
2023-08-18 11:29:13 +0530

    commit2

--- app1/models.py
+++ app1/models.py
@@ -1,41 +1,45 @@
 # Create your models here.
-from django.db import models 
+from django.db import models
 import uuid
+
 
 # Create your models here.
 class Customer(models.Model):
-     id = models.AutoField(primary_key=True)
-     name = models.CharField(max_length=100)
-     account_number = models.CharField(max_length=20, unique=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=20, unique=True)
 
-     def __str__(self):
-          return f'{self.id} {self.name} {self.account_number}'
-     
+    def __str__(self):
+        return f"{self.id} {self.name} {self.account_number}"
+
 
 class ServiceRequest(models.Model):
-     Service_Types = (
-          ('Gas Leak','Gas Leak'),
-          ('New Connection','New Connection'),
-          ('Replace','Replace'), 
-          #more Services  
-          )
-     tracking_no = models.UUIDField(primary_key=True,default=uuid.uuid4)
-     account = models.ForeignKey(Customer,on_delete=models.CASCADE)
-     customer = models.CharField(max_length=100)
-     Service_Type = models.CharField(max_length=50,choices=Service_Types)
-     request_detials = models.TextField()
-     attachment = models.FileField(upload_to='attachments/',blank=True, null=True)
-     submitted_at = models.DateTimeField(auto_now_add=True)
-     resolved_at = models.DateTimeField(blank=True,null=True)
-     status = models.CharField(max_length=50, default='Pending')
+    Service_Types = (
+        ("Gas Leak", "Gas Leak"),
+        ("New Connection", "New Connection"),
+        ("Replace", "Replace"),
+        # more Services
+    )
+    tracking_no = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    account = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.CharField(max_length=100)
+    Service_Type = models.CharField(max_length=50, choices=Service_Types)
+    request_detials = models.TextField()
+    attachment = models.FileField(upload_to="attachments/", blank=True, null=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    resolved_at = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=50, default="Pending")
 
-     # def __str__(self):
-     #      return self.customer
-     
+    # def __str__(self):
+    #      return self.customer
+
+
 class TrackStatus(models.Model):
-     tracking_no = models.ForeignKey(ServiceRequest,on_delete=models.CASCADE,related_name='status_updates') 
-     resolved_at = models.DateTimeField(blank=True,null=True)
-     status = models.CharField(max_length=50, default='Pending')
+    tracking_no = models.ForeignKey(
+        ServiceRequest, on_delete=models.CASCADE, related_name="status_updates"
+    )
+    resolved_at = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=50, default="Pending")
 
-     def __str__(self):
+    def __str__(self):
         return str(self.status)
--- app1/views.py
+++ app1/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from .models import Customer, ServiceRequest,TrackStatus
-from .serializer import CustomerSerializer, ServiceRequestSerializer,TrackStatusSerializer
+from .models import Customer, ServiceRequest, TrackStatus
+from .serializer import (
+    CustomerSerializer,
+    ServiceRequestSerializer,
+    TrackStatusSerializer,
+)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 
 class ServiceRequestViewSet(viewsets.ModelViewSet):
     queryset = ServiceRequest.objects.all()
@@ -16,37 +21,48 @@ class ServiceRequestViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        
+
         # Get the tracking number of the created service request
         tracking_no = serializer.instance.tracking_no
-        
+
         headers = self.get_success_headers(serializer.data)
         return Response(
             {"tracking_no": str(tracking_no)},
             status=status.HTTP_201_CREATED,
-            headers=headers
+            headers=headers,
         )
+
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer 
+    serializer_class = CustomerSerializer
+
 
 class TrackStatusView(APIView):
     def get(self, request, tracking_no):
         try:
             service_request = ServiceRequest.objects.get(tracking_no=tracking_no)
-             
+
             track_statuses = TrackStatus.objects.filter(tracking_no=service_request)
-             
-            
+
             if track_statuses:
-                status_list = [status_obj.status for status_obj in track_statuses] 
-                resolved_date = service_request.resolved_at.strftime("%Y-%m-%d %H:%M:%S") if service_request.resolved_at else "Not resolved yet"
-                return Response({"tracking_no": str(tracking_no), "statuses": status_list,"resolved_date":resolved_date})
+                status_list = [status_obj.status for status_obj in track_statuses]
+                resolved_date = (
+                    service_request.resolved_at.strftime("%Y-%m-%d %H:%M:%S")
+                    if service_request.resolved_at
+                    else "Not resolved yet"
+                )
+                return Response(
+                    {
+                        "tracking_no": str(tracking_no),
+                        "statuses": status_list,
+                        "resolved_date": resolved_date,
+                    }
+                )
             else:
                 return Response({"status": "Pending"})
         except ServiceRequest.DoesNotExist:
             return Response(
                 {"error": "Tracking number does not exist"},
-                status=status.HTTP_404_NOT_FOUND
+                status=status.HTTP_404_NOT_FOUND,
             )
